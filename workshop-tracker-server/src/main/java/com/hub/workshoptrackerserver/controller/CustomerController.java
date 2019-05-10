597f6625948a7156f7263a6cbf20823e44f73a09
mramoss <moi6ramoss@gmail.com>
2019-05-09 21:56:01 -0400

    Add api to register a customer

--- workshop-tracker-server/src/main/java/com/hub/workshoptrackerserver/controller/CustomerController.java
+++ workshop-tracker-server/src/main/java/com/hub/workshoptrackerserver/controller/CustomerController.java
@@ -3,11 +3,77 @@ package com.hub.workshoptrackerserver.controller;
 import com.hub.workshoptrackerserver.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("customers")
 public class CustomerController {
     @Autowired
     CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/add")
+    public void registerCustomer(@RequestBody CustomerController.RequetsCustomer newCustomer) {
+        customerService.registerCustomerService(newCustomer);
+
+    }
+
+    public static class RequetsCustomer {
+
+        private Long Id;
+        private String name;
+        private String lastName;
+        private String email;
+        private Integer cellPhoneNumber;
+        private Integer phoneNumber;
+
+        public Long getId() {
+            return Id;
+        }
+
+        public void setId(Long id) {
+            Id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Integer getCellPhoneNumber() {
+            return cellPhoneNumber;
+        }
+
+        public void setCellPhoneNumber(Integer cellPhoneNumber) {
+            this.cellPhoneNumber = cellPhoneNumber;
+        }
+
+        public Integer getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(Integer phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
 }
--- workshop-tracker-server/src/main/java/com/hub/workshoptrackerserver/service/CustomerService.java
+++ workshop-tracker-server/src/main/java/com/hub/workshoptrackerserver/service/CustomerService.java
@@ -1,6 +1,8 @@
 package com.hub.workshoptrackerserver.service;
 
+import com.hub.workshoptrackerserver.controller.CustomerController;
 import com.hub.workshoptrackerserver.dao.CustomerRepository;
+import com.hub.workshoptrackerserver.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,14 @@ import org.springframework.stereotype.Service;
 public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
+
+    public void registerCustomerService(CustomerController.RequetsCustomer newCustomer) {
+        Customer customer = new Customer();
+        customer.setId(newCustomer.getId());
+        customer.setName(newCustomer.getName());
+        customer.setLastName(newCustomer.getLastName());
+        customer.setCellPhoneNumber(newCustomer.getCellPhoneNumber());
+        customer.setPhoneNumber(newCustomer.getPhoneNumber());
+        customerRepository.save(customer);
+    }
 }
